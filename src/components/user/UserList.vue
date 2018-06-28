6cb27fffcb32532dbb647561d00eda4603af6a40
Moekr <highter@outlook.com>
2018-06-28 19:15:05 +0800

    增加管理员重置用户密码的功能

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -7,14 +7,14 @@
           <el-button type="primary" icon="el-icon-refresh" @click="fetchData(pageInfo.page)">刷新</el-button>
         </div>
       </div>
-      <el-dialog title="确认删除用户" :visible.sync="dialog">
-        <b>确认删除用户吗？</b>
+      <el-dialog :title="'确认'+action" :visible.sync="dialog">
+        <b>确认{{ action }}吗？</b>
         <p>用户ID：{{ selectedUser.id }}</p>
         <p>用户名：{{ selectedUser.username }}</p>
         <p>用户邮箱：{{ selectedUser.email }}</p>
         <span slot="footer" class="dialog-footer">
           <el-button @click="dialog = false">取消</el-button>
-          <el-button type="danger" @click="deleteUser" :loading="loading">确定</el-button>
+          <el-button :type="buttonType" @click="performAction" :loading="loading">确定</el-button>
         </span>
       </el-dialog>
       <div class="fill-card">
@@ -32,9 +32,10 @@
               <span>{{ scope.row.role | role }}</span>
             </template>
           </el-table-column>
-          <el-table-column label="操作" width="150">
+          <el-table-column label="操作" width="200">
             <template slot-scope="scope">
-              <el-button type="danger" size="mini" @click="selectUser(scope.row)">删除用户</el-button>
+              <el-button type="warning" size="mini" @click="selectUser(scope.row, '重置密码')">重置密码</el-button>
+              <el-button type="danger" size="mini" @click="selectUser(scope.row, '删除用户')">删除用户</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -64,22 +65,53 @@ export default {
         })
       })
     },
-    selectUser (user) {
+    selectUser (user, action) {
       this.selectedUser = user
+      this.action = action
       this.dialog = true
+    },
+    performAction () {
+      this.loading = true
+      if (this.action === '重置密码') {
+        this.resetPassword()
+      } else if (this.action === '删除用户') {
+        this.deleteUser()
+      }
+      this.loading = false
+    },
+    resetPassword () {
+      this.$http.post('./api/user/' + this.selectedUser.id + '/password/reset').then(response => {
+        this.$message.success({
+          message: '重置密码成功！',
+          center: true
+        })
+        this.dialog = false
+      }, response => {
+        this.$message.error({
+          message: Tool.errorMessage(response),
+          center: true
+        })
+      })
     },
     deleteUser () {
       this.$http.delete('./api/user/' + this.selectedUser.id).then(response => {
         this.dialog = false
-        this.loading = false
         this.fetchData(this.pageInfo.page)
       }, response => {
         this.$message.error({
           message: Tool.errorMessage(response),
           center: true
         })
-        this.loading = false
       })
+    }
+  },
+  computed: {
+    buttonType: function () {
+      if (this.action === '重置密码') {
+        return 'warning'
+      } else {
+        return 'danger'
+      }
     }
   },
   filters: {
@@ -102,6 +134,7 @@ export default {
       pageInfo: {},
       userList: [],
       selectedUser: {},
+      action: '',
       dialog: false,
       loading: false
     }
